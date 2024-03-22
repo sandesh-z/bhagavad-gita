619d308e3910be99522e38c56e6d2b146fc16c35
sandesh <gsandace@gmail.com>
2024-03-22 21:28:59 +0545

    removed unused package

--- lib/features/providers/home_bloc/home_bloc.dart
+++ lib/features/providers/home_bloc/home_bloc.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 
 import 'package:bhagavad_gita/features/datasource/bhagavad_remote_data_source.dart';
 import 'package:bhagavad_gita/features/utils/generate_random_chapter_with_verse.dart';
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 
